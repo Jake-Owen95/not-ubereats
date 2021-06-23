0913d45349129e1f3914991ab2a7c0dd875c8a1f
Jake <jakejohnowen@gmail.com>
2021-06-23 13:39:21 +0200

    footer fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,6 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => (
   <div className="footer mt-4 mb-4">
     <div className="text-center">
-      Site by 
-      <FontAwesomeIcon color="red" icon={faHeart} />
       Website by{' '}
       <a href="https://www.linkedin.com/in/jake-owen-196960a8/" target="_blank" rel="noreferrer">
         Jake Owen {' '} 
