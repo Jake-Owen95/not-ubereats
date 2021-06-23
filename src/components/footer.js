68b7a2384d8a825a09254e4a596982f81ae0088f
Jake <jakejohnowen@gmail.com>
2021-06-23 13:35:58 +0200

    Footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,16 +6,12 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => (
   <div className="footer mt-4 mb-4">
     <div className="text-center">
-      Made with&nbsp;
+      Site by 
       <FontAwesomeIcon color="red" icon={faHeart} />
-      &nbsp;by{' '}
-      <a href="https://www.linkedin.com/in/randynsingh/" target="_blank" rel="noreferrer">
-        Randy Singh {' '} 
+      Website by{' '}
+      <a href="https://www.linkedin.com/in/jake-owen-196960a8/" target="_blank" rel="noreferrer">
+        Jake Owen {' '} 
       </a>
-      and brought to you by{' '}
-        <a href="https://www.linkedin.com/in/jake-owen-196960a8/" target="_blank" rel="noreferrer">
-          Jake Owen {' '}
-        </a>
       <div>
         <div>
           Images are property of their respective owners.
